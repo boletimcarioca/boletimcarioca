5187852c55dd3acf06f85f0a8e7bd7381d0433d9
boletimcarioca <boletimcarioca@outlook.com>
2026-08-09 20:58:32 -0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/*
+ * BOLETIM CARIOCA
+ * Aplicação principal
+ *
+ * Este arquivo será ampliado progressivamente
+ * durante a construção do projeto.
+ */
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Boletim Carioca iniciado.");
+});
